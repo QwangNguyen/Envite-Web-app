c39cbdfd913d9092f6fbc75bff0eac9c921e59ce
Kaarina <k1j20@tulleau.com>
2022-03-14 11:42:35 -0700

    Changed redundant code to .map()

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,6 @@
 import React from 'react'; 
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import { Login } from './login';
-<<<<<<< HEAD
-// import { propTypes } from 'react-bootstrap/esm/Image';
-=======
->>>>>>> a9b939f4631d0eaf0dd334dc14f857db5a168da1
 
 export function Dashboard(props) {
     let dashboardInfo = [{"cardTitle" : "CO2 Generated in One Year", "cardDisplay": props.scoreData[0]["co2/year"] + " lbs"},
@@ -26,26 +22,9 @@ export function Dashboard(props) {
         return(
             <main>
                 <h1 className='text-center bold greeting'>Hello User</h1>
-<<<<<<< HEAD
-                {/* Profile Picutre */}
-                <div className='container spaceBetween centerElement' aria-label="Dashboard">
-                    <div className='dashboardCard'>
-                        <p className='bold titleCard'>CO2 Generated in One year:</p>
-                        <p className='dataDisplay'>1350lbs</p>
-                    </div>
-                    <div className='dashboardCard'>
-                        <p className='bold titleCard'>Sustainability Score:</p>
-                        <p className='dataDisplay'>40</p>
-                    </div>
-                    <div className='dashboardCard'>
-                        <p className='bold titleCard'>Equivalent Earths Used:</p>
-                        <p className='dataDisplay'>2.5</p>
-                    </div>
-=======
                 {/* Profile Picture */}
                 <div className='container spaceBetween centerElement'>
                     {displayCards}    
->>>>>>> a9b939f4631d0eaf0dd334dc14f857db5a168da1
                 </div>
                 <div className='dashboardCard long'>
                     <p className='bold titleCard'>Earth Day:</p>

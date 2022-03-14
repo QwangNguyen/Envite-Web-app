c5b52675851c009b08d0c2e479bbb5234d31f4ac
Kaarina <k1j20@tulleau.com>
2022-03-13 23:53:45 -0700

    Replaced repetitive code with .map()

--- src/components/TriviaCategory.js
+++ src/components/TriviaCategory.js
@@ -2,11 +2,36 @@ import React from 'react';
 import { Link} from 'react-router-dom';
 
 export function TriviaCategory(props) {
-    return (
-        <main className="centerMain">
-            <div className="container containerTrivia">
-                <div className="row">
-                    <div className="col-md-4 d-flex centerElement">
+
+    let catInfoArr = [{"catNum": "21", "catName": "Animals of the Earth"},
+                  {"catNum": "9919", "catName": "Nature Study"},
+                  {"catNum": "1215", "catName": "Climate Change"},
+                  {"catNum": "267", "catName": "Wildlife Ecology"},
+                  {"catNum": "3835", "catName": "Endangered Species"},
+                  {"catNum": "Random", "catName": "Random"}];
+
+    const rows = catInfoArr.map((cat, index) => {
+        if (index % 2 === 0) {
+            return (
+                <div className="col-md-4 d-flex centerElement">
+                    <div className="buttonbody">
+                        <Link to="/triviaQuestion" onClick={() => props.setCatCallback(cat.catNum)} className="btn btn-lg triviaCategory">{cat.catName}</Link>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="col-md-4 d-flex centerElement">
+                    <div className="buttonbody">
+                        <Link to="/triviaQuestion" onClick={() => props.setCatCallback(cat.catNum)} className="btn btn-lg triviaCategory buttonTan">{cat.catName}</Link>
+                    </div>
+                </div>
+            )
+        }
+    });
+
+    /*
+    <div className="col-md-4 d-flex centerElement">
                         <div className="buttonbody">
                             <Link to="/triviaQuestion" onClick={() => props.setCatCallback("21")} className="btn btn-lg triviaCategory">Animals of the Earth</Link>
                         </div>
@@ -36,6 +61,13 @@ export function TriviaCategory(props) {
                             <Link to="/triviaQuestion" onClick={() => props.setCatCallback("Random")} className="btn btn-lg triviaCategory buttonTan">Random</Link>
                         </div>
                     </div>
+    */
+    
+    return (
+        <main className="centerMain">
+            <div className="container containerTrivia">
+                <div className="row">
+                    {rows}
                 </div>
             </div>
         </main>

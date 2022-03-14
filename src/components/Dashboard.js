84e49f0ad1347e35572d10fd2ef71bfdda21fd7f
Quang Nguyen <simplyquang@gmail.com>
2022-03-14 14:24:42 -0700

    finished sustainability score

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { Login } from './login';
 
 export function Dashboard(props) {
     let dashboardInfo = [{"cardTitle" : "CO2 Generated in One Year", "cardDisplay": props.scoreData[0]["co2/year"] + " lbs"},
-                        {"cardTitle" : "Sustainability Score:", "cardDisplay": props.scoreData[0]["sustainabilityScore"]},
+                        {"cardTitle" : "Sustainability Score:", "cardDisplay": props.scoreData[0]["sustainabilityScore"]*100},
                         {"cardTitle" : "Equivalent Earths Used:", "cardDisplay": props.scoreData[0]["equivalentEarths"]}];
 
     const displayCards = dashboardInfo.map((card, index) => {
@@ -24,11 +24,6 @@ export function Dashboard(props) {
                 <h1 className='text-center bold greeting'>Hello User</h1>
                 <div className='container spaceBetween centerElement'>
                     {displayCards}    
-                </div>
-                <div className='dashboardCard long'>
-                    <p className='bold titleCard'>Earth Day:</p>
-                    <p className='dataDisplay'>(Represent the date that earth's yearly carbon threshold is passed if everyone lives the same way as you)</p>
-                    <p className='dataDisplay'> March 25th</p>
                 </div>
                 <p className='bold titleCard firstTitleCard'>Questions answered correct: {props.score[0]} / {props.score[1]}</p>
                 <ProgressBar variant="success" animated now={props.score[0]} max={props.score[1]} label={(props.score[1] === 0 ? '0' : props.score[0] / props.score[1] * 100 + '%')} className='barChart' />
--- src/components/SustainabilityScore.js
+++ src/components/SustainabilityScore.js
@@ -4,9 +4,12 @@ export function SustainabilityScore(props) {
     return(    
         <main className="inputMain">
             <h1 className="titleOfPage">Sustainability Score</h1>
-            <p className="score">90</p>
-            <div className="containerInput container" role="button">
-            <button type="button" className="btn btn-lg buttonCustom roundButton">Analyze</button>
+            <p className="score">{props.scoreData[0]["sustainabilityScore"] === null ? 0 : (props.scoreData[0]["sustainabilityScore"] * 100).toFixed(3)} </p>
+            <div className="container" role="button">
+            <p className="scoreText"> Sustainability score is a score that takes into consideration how much Co2 you produce 
+                relative to how much Co2 the Earth can sustain for an average person. A score of 100 represents
+                that the Earth could sustain if every person produced the same amount as you. Any score less
+                means that the Earth could not sustain every person living like you. A higher score is better.</p>
             </div>
         </main>
     );

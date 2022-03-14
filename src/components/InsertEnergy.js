10f946de70534d74ff334991cebc261cfbc5358c
Kaarina <k1j20@tulleau.com>
2022-03-14 10:40:44 -0700

    Removed repetitive code using .map()

--- src/components/InsertEnergy.js
+++ src/components/InsertEnergy.js
@@ -1,42 +1,29 @@
 import React from 'react';
 
 export function InsertEnergy(props) {
-    return(
-        <form>
-          <div>
-            <label htmlFor="naturalGas" className="formInput labelForm">Natural Gas Usage per Month:</label><br/>
-            <input type="number" name="naturalGas" onChange={props.changeCallback} value={props.currValue[0]["naturalGas"]} id="naturalGas" className="textBox formInput"/>
-            <select className="selectForm" onChange={props.changeCallback} name="gasOption" value={props.currValue[0]["gasOption"]}>
-              <option value="dollars">Dollars</option>
-              <option value="therms">Therms</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="electricity" className="formInput labelForm">Electricity per Month:</label><br/>
-            <input type="number" name="electricity" onChange={props.changeCallback} value={props.currValue[0]["electricity"]} id ="electricity" className="textBox formInput"/>
-            <select className="selectForm" onChange={props.changeCallback} name="electricityOption" value={props.currValue[0]["electricityOption"]}>
-              <option value="dollars">Dollars</option>
-              <option value="kWh">kWh</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="fuelOil" className="formInput labelForm">Fuel Oil per Month:</label><br/>
-            <input type="number" name="fuelOil" onChange={props.changeCallback} value={props.currValue[0]["fuelOil"]} id="fuelOil" className="textBox formInput"/>
-            <select className="selectForm" onChange={props.changeCallback} name="fuelOption" value={props.currValue[0]["fuelOption"]}>
-              <option value="dollars">Dollars</option>
-              <option value="gallons">Gallons</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="propane" className="formInput labelForm">Propane per Month:</label><br/>
-            <input type="number" name="propane" onChange={props.changeCallback} value={props.currValue[0]["propane"]} id="propane" className="textBox formInput"/> 
-            <select className="selectForm" onChange={props.changeCallback} name="propaneOption" value={props.currValue[0]["propaneOption"]}>
-              <option value="dollars">Dollars</option>
-              <option value="gallons">Gallons</option>
-            </select>
-          </div>
-        </form>
-    );
+  let formInfo = [{"name": "naturalGas", "option": "gasOption", "text": "Natural Gas Usage per Month:", "unit": "therms", "unitText": "Therms"},
+                  {"name": "electricity", "option": "electricityOption", "text": "Electricity per Month:", "unit": "kWh", "unitText": "kWh"},
+                  {"name": "fuelOil", "option": "fuelOption", "text": "Fuel Oil per Month:", "unit": "gallons", "unitText": "Gallons"},
+                  {"name": "propane", "option": "propaneOption", "text": "Propane per Month:", "unit": "gallons", "unitText": "Gallons"}];
+  
+  const formDivs = formInfo.map((div) => {
+    return (
+      <div>
+        <label htmlFor={div.name} className="formInput labelForm">{div.text}</label><br/>
+        <input type="number" name={div.name} onChange={props.changeCallback} value={props.currValue[0][div.name]} id={div.name} className="textBox formInput"/>
+        <select className="selectForm" onChange={props.changeCallback} name={div.option} value={props.currValue[0][div.name]}>
+          <option value="dollars">Dollars</option>
+          <option value={div.unit}>{div.unitText}</option>
+        </select>
+      </div>
+    )
+  });
+
+  return(
+      <form>
+        {formDivs}
+      </form>
+  );
 }
 
 

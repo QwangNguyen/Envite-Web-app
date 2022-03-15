fca284d35f74a4660a81bb161af12ea642c4c962
vivian <pwy8900@uw.edu>
2022-03-14 20:24:53 -0700

    css piechart

--- src/components/SustainabilityScore.js
+++ src/components/SustainabilityScore.js
@@ -9,7 +9,7 @@ export function SustainabilityScore(props) {
     return(    
         <main className="inputMain">
             <h1 className="titleOfPage">Sustainability Score</h1>
-            <div className="col-md-6 d-flex">
+            <div className="d-flex piechart">
                 <PieChart
                 data={[
                     { title: 'Vehicle C02', value: vehiclePortion, color: "#7F9183"},
@@ -21,7 +21,7 @@ export function SustainabilityScore(props) {
                 <PieChart />
             </div>
             <p className="score">{isNaN(props.scoreData[0]["sustainabilityScore"]) ? 0 : (props.scoreData[0]["sustainabilityScore"] * 100).toFixed(3)} </p>
-            <div className="container" role="button">
+            <div className="scoreContainer" role="button">
             <p className="scoreText"> Sustainability score is a score that takes into consideration how much Co2 you produce 
                 relative to how much Co2 the Earth can sustain for an average person. A score of 100 represents
                 that the Earth could sustain if every person produced the same amount as you. Any score less

2383ef360e569fd1e8a4678d2798012e866e77d9
Quang Nguyen <simplyquang@gmail.com>
2022-03-14 21:54:36 -0700

    played around with some settings

--- src/components/SustainabilityScore.js
+++ src/components/SustainabilityScore.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 
 export function SustainabilityScore(props) {
@@ -6,30 +6,47 @@ export function SustainabilityScore(props) {
     let vehiclePortion = props.scoreData[0]["vehiclePortion"];
     let energyPortion = props.scoreData[0]["energyPortion"];
 
-    return(    
-        <main className="inputMain">
-            <h1 className="titleOfPage">Sustainability Score</h1>
-            <div className="scoreAndChart">
-            <div className="piechart">
-                <PieChart
-                data={[
-                    { title: 'Vehicle C02', value: vehiclePortion, color: "#7F9183"},
-                    { title: 'Energy C02', value: energyPortion, color: "#CFC0BD"}
-                ]}
-                label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %`}
-                labelStyle = {{
-                    fontFamily: 'Roboto'}}/>
-                <PieChart />
-            </div>
-            <p className="score">{isNaN(props.scoreData[0]["sustainabilityScore"]) ? 0 : (props.scoreData[0]["sustainabilityScore"] * 100).toFixed(3)} </p>
-            </div>
+    const[isChart, setIsChart] = useState(false);
 
-            <div className="scoreContainer" role="button">
-            <p className="scoreText"> Sustainability score is a score that takes into consideration how much Co2 you produce 
-                relative to how much Co2 the Earth can sustain for an average person. A score of 100 represents
-                that the Earth could sustain if every person produced the same amount as you. Any score less
-                means that the Earth could not sustain every person living like you. A higher score is better.</p>
-            </div>
-        </main>
-    );
+    const toggleChart = () => {
+        console.log("toggle");
+        setIsChart(!isChart);
+    }
+
+    if(isChart) {
+        return (
+            <main className="inputMain">
+                <h1 className="titleOfPage">Sustainability Score</h1>
+                
+                <div className="piechart">
+                    <PieChart
+                    data={[
+                            { title: 'Vehicle C02', value: vehiclePortion, color: "#7F9183"},
+                            { title: 'Energy C02', value: energyPortion, color: "#CFC0BD"}
+                        ]}
+                        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %`}
+                        labelStyle = {{
+                        fontFamily: 'Roboto'}} className="pieChart"/>
+                    <PieChart />
+                </div>
+                <button onClick={() => toggleChart}>Here</button>
+            </main>
+        );
+    } else {
+        return(    
+            <main className="inputMain">
+                <h1 className="titleOfPage">Sustainability Score</h1>
+                <p className="score">{isNaN(props.scoreData[0]["sustainabilityScore"]) ? 0 : (props.scoreData[0]["sustainabilityScore"] * 100).toFixed(3)} </p>
+
+
+                <div className="scoreContainer" role="button">
+                <p className="scoreText"> Sustainability score is a score that takes into consideration how much Co2 you produce 
+                    relative to how much Co2 the Earth can sustain for an average person. A score of 100 represents
+                    that the Earth could sustain if every person produced the same amount as you. Any score less
+                    means that the Earth could not sustain every person living like you. A higher score is better.</p>
+                </div>
+                <button onClick={() => toggleChart}>Here</button>
+            </main>
+        );
+    }
 }
